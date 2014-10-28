ede8683aba679a5685b360310bec95a1aa55c351
Juan A. Moyano <wincus@gmail.com>
2014-10-28 18:25:20 -0300

    ups, read only access. Try setting https access.

--- update-pdf-file.sh
+++ update-pdf-file.sh
@@ -4,8 +4,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
-  git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+  git config credential.helper "store --file=.git/credentials"
+  git remote set-url origin https://github.com/wincus/curric.git
 
   git add pdf/curric.pdf
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [ci skip]"
