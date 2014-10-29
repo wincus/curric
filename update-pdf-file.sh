fd7fff5eed451073d31f2ce0d935e05efe05d605
Juan A. Moyano <wincus@gmail.com>
2014-10-29 10:45:14 -0300

    works now? lets see ...

--- update-pdf-file.sh
+++ update-pdf-file.sh
@@ -1,16 +1,16 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Uploading pdf version ...\n"
-
+  
+  git checkout master
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
 
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
   git config credential.helper "store --file=.git/credentials"
   git remote set-url origin https://github.com/wincus/curric.git
-
   git add pdf/curric.pdf
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [ci skip]"
-  git push -fq origin master
+  git push -fq origin master > /dev/null
 
   echo -e "Done!\n"
 fi
